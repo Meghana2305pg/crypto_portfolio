8a3c3e5c7424cdcd692ef3c6725360ea0d04dee4
Meghana2305pg <meghanapg03@gmail.com>
2026-02-16 16:48:30 +0530

    Configured static files with WhiteNoise

--- crypto_portfolio/settings.py
+++ crypto_portfolio/settings.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'crypto_portfolio.urls'
@@ -69,6 +70,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 CACHES = {
     'default': {
