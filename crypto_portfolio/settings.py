99f3b97340552c9df9288b1eac6d3b9ff9cef6bc
Meghana2305pg <meghanapg03@gmail.com>
2026-02-16 16:20:07 +0530

    Fixed allowed hosts for Render

--- crypto_portfolio/settings.py
+++ crypto_portfolio/settings.py
@@ -5,7 +5,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'replace-with-a-secure-secret-key'
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'coinscope-5rze.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
